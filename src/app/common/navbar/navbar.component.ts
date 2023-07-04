b1b1bb56008fb7642f9fb88281ff7ec4f32c515d
Kairucxz <kylemarin05@gmail.com>
2023-07-04 17:33:09 +0800

    finished sidebar & navbar

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   currentDate: Date = new Date();
+  selected: string | undefined;
+  constructor() { }
 
   ngOnInit() {
     setInterval(() => {
       this.currentDate = new Date();
     }, 1000);
+
+    this.selected = 'Dashboard'
   }
 }
