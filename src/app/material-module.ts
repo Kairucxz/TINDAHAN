8530df82a909d2ba1cd3a22f739acf37a4fd8131
Kairucxz <kylemarin05@gmail.com>
2023-07-05 17:22:24 +0800

    fixed sidebar css

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -4,6 +4,7 @@ import {MatIconModule} from "@angular/material/icon"
 import {MatButtonModule} from "@angular/material/button"
 import {MatSidenavModule} from "@angular/material/sidenav"
 import {MatListModule} from "@angular/material/list"
+import { MatExpansionModule } from "@angular/material/expansion"
 import { NgIf } from "@angular/common";
 
 
@@ -14,6 +15,7 @@ exports:[
     MatButtonModule,
     MatSidenavModule,
     MatListModule,
+    MatExpansionModule,
     
 
 
